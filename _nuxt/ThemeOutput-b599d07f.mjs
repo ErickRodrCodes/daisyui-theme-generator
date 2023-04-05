123f44e42db88630b43501ed9bfcc84c02345e60
Erick Rodriguez <tbogard@outlook.com>
2023-04-05 16:31:45 -0400

    first batch

---
+++ _nuxt/ThemeOutput-b599d07f.mjs
@@ -0,0 +1,44 @@
+import{_ as l,d,c as _,e as n,h as s,t as c,j as e,p as r,k as u,o as m}from"./entry-dc391e8a.mjs";const h=d({props:{name:{type:String},isDefault:{type:Boolean,default:!1},config:{type:Object,required:!0}},setup(a,{expose:i}){i();const o={};return Object.defineProperty(o,"__isScriptSetup",{enumerable:!1,value:!0}),o}}),t=a=>(r("data-v-54a9a8d6"),a=a(),u(),a),g={class:"w-full max-w-5xl space-y-4"},f=t(()=>n("h2",null,[s("Copy the below theme configuration into "),n("span",{class:"kbd"},"tailwind.config.js")],-1)),b={class:"shadow-lg mockup-code"},y=t(()=>n("span",{class:"comment"},"           /* Primary color */",-1)),v=s(`
+          `),w=t(()=>n("span",{class:"comment"},"     /* Primary color - focused */",-1)),k=s(`
+          `),x=t(()=>n("span",{class:"comment"},"   /* Foreground content color to use on primary color */",-1)),C=s(`
+
+          `),F=t(()=>n("span",{class:"comment"},"         /* Secondary color */",-1)),B=s(`
+          `),S=t(()=>n("span",{class:"comment"},"   /* Secondary color - focused */",-1)),I=s(`
+          `),j=t(()=>n("span",{class:"comment"}," /* Foreground content color to use on secondary color */",-1)),N=s(`
+
+          `),P=t(()=>n("span",{class:"comment"},"            /* Accent color */",-1)),O=s(`
+          `),T=t(()=>n("span",{class:"comment"},"      /* Accent color - focused */",-1)),z=s(`
+          `),A=t(()=>n("span",{class:"comment"},"    /* Foreground content color to use on accent color */",-1)),D=s(`
+
+          `),E=t(()=>n("span",{class:"comment"},"           /* Neutral color */",-1)),V=s(`
+          `),q=t(()=>n("span",{class:"comment"},"     /* Neutral color - focused */",-1)),W=s(`
+          `),G=t(()=>n("span",{class:"comment"},"   /* Foreground content color to use on neutral color */",-1)),H=s(`
+
+          `),J=t(()=>n("span",{class:"comment"},"          /* Base color of page, used for blank backgrounds */",-1)),K=s(`
+          `),L=t(()=>n("span",{class:"comment"},"          /* Base color, a little darker */",-1)),M=s(`
+          `),Q=t(()=>n("span",{class:"comment"},"          /* Base color, even more darker */",-1)),R=s(`
+          `),U=t(()=>n("span",{class:"comment"},"      /* Foreground content color to use on base color */",-1)),X=s(`
+
+          `),Y=t(()=>n("span",{class:"comment"},"              /* Info */",-1)),Z=s(`
+          `),p=t(()=>n("span",{class:"comment"},"           /* Success */",-1)),$=s(`
+          `),nn=t(()=>n("span",{class:"comment"},"           /* Warning */",-1)),sn=s(`
+          `),on=t(()=>n("span",{class:"comment"},"             /* Error */",-1)),tn=s(`
+
+          `),cn=t(()=>n("div",{class:"mini-swatch bg-neutral"},null,-1)),en=t(()=>n("span",{class:"comment"},"/* border-radius for cards and other big elements */",-1)),an=s(`
+          `),ln=t(()=>n("div",{class:"mini-swatch bg-neutral"},null,-1)),dn=t(()=>n("span",{class:"comment"},"/* border-radius for buttons and similar elements */",-1)),_n=s(`
+          `),rn=t(()=>n("div",{class:"mini-swatch bg-neutral"},null,-1)),un=t(()=>n("span",{class:"comment"},"/* border-radius for badge and other small elements */",-1)),mn=s(`
+
+          `),hn=t(()=>n("div",{class:"mini-swatch bg-neutral"},null,-1)),gn=t(()=>n("span",{class:"comment"},"/* bounce animation time for button */",-1)),fn=s(`
+          `),bn=t(()=>n("div",{class:"mini-swatch bg-neutral"},null,-1)),yn=t(()=>n("span",{class:"comment"},"/* bounce animation time for checkbox, toggle, etc */",-1)),vn=s(`
+
+          `),wn=t(()=>n("div",{class:"mini-swatch bg-neutral"},null,-1)),kn=t(()=>n("span",{class:"comment"},"/* default text case for buttons */",-1)),xn=s(`
+          `),Cn=t(()=>n("div",{class:"mini-swatch bg-neutral"},null,-1)),Fn=t(()=>n("span",{class:"comment"},"/* default padding for navbar */",-1)),Bn=s(`
+          `),Sn=t(()=>n("div",{class:"mini-swatch bg-neutral"},null,-1)),In=t(()=>n("span",{class:"comment"},"/* default border size for button */",-1)),jn=s(`
+        },
+      },
+    ],
+  },`);function Nn(a,i,o,Pn,On,Tn){return m(),_("div",g,[f,n("div",b,[n("pre",null,[n("code",null,[s(`daisyui: {
+    themes: [
+      {
+        '`+c(o.name)+`': {
+          `,1),n("div",{class:"mini-swatch",style:e(`background: ${o.config.primary}`)},null,4),s(" 'primary' : '"+c(o.config.primary)+"',",1),y,v,n("div",{class:"mini-swatch",style:e(`background: ${o.config.primaryFocus}`)},null,4),s(" 'primary-focus' : '"+c(o.config.primaryFocus)+"',",1),w,k,n("div",{class:"mini-swatch",style:e(`background: ${o.config.primaryContent}`)},null,4),s(" 'primary-content' : '"+c(o.config.primaryContent)+"',",1),x,C,n("div",{class:"mini-swatch",style:e(`background: ${o.config.secondary}`)},null,4),s(" 'secondary' : '"+c(o.config.secondary)+"',",1),F,B,n("div",{class:"mini-swatch",style:e(`background: ${o.config.secondaryFocus}`)},null,4),s(" 'secondary-focus' : '"+c(o.config.secondaryFocus)+"',",1),S,I,n("div",{class:"mini-swatch",style:e(`background: ${o.config.secondaryContent}`)},null,4),s(" 'secondary-content' : '"+c(o.config.secondaryContent)+"',",1),j,N,n("div",{class:"mini-swatch",style:e(`background: ${o.config.accent}`)},null,4),s(" 'accent' : '"+c(o.config.accent)+"',",1),P,O,n("div",{class:"mini-swatch",style:e(`background: ${o.config.accentFocus}`)},null,4),s(" 'accent-focus' : '"+c(o.config.accentFocus)+"',",1),T,z,n("div",{class:"mini-swatch",style:e(`background: ${o.config.accentContent}`)},null,4),s(" 'accent-content' : '"+c(o.config.accentContent)+"',",1),A,D,n("div",{class:"mini-swatch",style:e(`background: ${o.config.neutral}`)},null,4),s(" 'neutral' : '"+c(o.config.neutral)+"',",1),E,V,n("div",{class:"mini-swatch",style:e(`background: ${o.config.neutralFocus}`)},null,4),s(" 'neutral-focus' : '"+c(o.config.neutralFocus)+"',",1),q,W,n("div",{class:"mini-swatch",style:e(`background: ${o.config.neutralContent}`)},null,4),s(" 'neutral-content' : '"+c(o.config.neutralContent)+"',",1),G,H,n("div",{class:"mini-swatch",style:e(`background: ${o.config.base100}`)},null,4),s(" 'base-100' : '"+c(o.config.base100)+"',",1),J,K,n("div",{class:"mini-swatch",style:e(`background: ${o.config.base200}`)},null,4),s(" 'base-200' : '"+c(o.config.base200)+"',",1),L,M,n("div",{class:"mini-swatch",style:e(`background: ${o.config.base300}`)},null,4),s(" 'base-300' : '"+c(o.config.base300)+"',",1),Q,R,n("div",{class:"mini-swatch",style:e(`background: ${o.config.baseContent}`)},null,4),s(" 'base-content' : '"+c(o.config.baseContent)+"',",1),U,X,n("div",{class:"mini-swatch",style:e(`background: ${o.config.info}`)},null,4),s(" 'info' : '"+c(o.config.info)+"',",1),Y,Z,n("div",{class:"mini-swatch",style:e(`background: ${o.config.success}`)},null,4),s(" 'success' : '"+c(o.config.success)+"',",1),p,$,n("div",{class:"mini-swatch",style:e(`background: ${o.config.warning}`)},null,4),s(" 'warning' : '"+c(o.config.warning)+"',",1),nn,sn,n("div",{class:"mini-swatch",style:e(`background: ${o.config.error}`)},null,4),s(" 'error' : '"+c(o.config.error)+"',",1),on,tn,cn,s("'--rounded-box': '"+c(o.config.roundedBox)+"',          ",1),en,an,ln,s("'--rounded-btn': '"+c(o.config.roundedBtn)+"',        ",1),dn,_n,rn,s("'--rounded-badge': '"+c(o.config.roundedBadge)+"',      ",1),un,mn,hn,s("'--animation-btn': '"+c(o.config.animationBtn)+"',       ",1),gn,fn,bn,s("'--animation-input': '"+c(o.config.animationInput)+"',       ",1),yn,vn,wn,s("'--btn-text-case': '"+c(o.config.btnTextCase)+"',   ",1),kn,xn,Cn,s("'--navbar-padding': '"+c(o.config.navbarPadding)+"',      ",1),Fn,Bn,Sn,s("'--border-btn': '"+c(o.config.borderBtn)+"',            ",1),In,jn])])])])}var An=l(h,[["render",Nn],["__scopeId","data-v-54a9a8d6"]]);export{An as default};
